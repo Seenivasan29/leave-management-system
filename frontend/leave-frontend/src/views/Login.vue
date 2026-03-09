dce7023ef98622bb532c35ecf67a27c1334b1e99
Seenivasan M J <vseeni185@gmail.com>
2026-03-09 14:59:32 +0530

    Updated API URL for production

--- frontend/leave-frontend/src/views/Login.vue
+++ frontend/leave-frontend/src/views/Login.vue
@@ -1,46 +1,44 @@
 <template>
+  <div class="min-h-screen flex items-center justify-center bg-gray-100">
 
-<div class="min-h-screen flex items-center justify-center bg-gray-100">
+    <div class="bg-white p-10 rounded-xl shadow-md w-96">
 
-<div class="bg-white p-10 rounded-xl shadow-md w-96">
+      <h1 class="text-2xl font-bold text-center text-gray-800 mb-2">
+        Leave Management
+      </h1>
 
-<h1 class="text-2xl font-bold text-center text-gray-800 mb-2">
-Leave Management
-</h1>
+      <p class="text-sm text-gray-500 text-center mb-6">
+        Sign in to your account
+      </p>
 
-<p class="text-sm text-gray-500 text-center mb-6">
-Sign in to your account
-</p>
+      <div class="space-y-4">
 
-<div class="space-y-4">
+        <input
+          v-model="email"
+          type="email"
+          placeholder="Email address"
+          class="w-full border rounded-lg p-2 focus:ring-2 focus:ring-blue-400 outline-none"
+        />
 
-<input
-v-model="email"
-type="email"
-placeholder="Email address"
-class="w-full border rounded-lg p-2 focus:ring-2 focus:ring-blue-400 outline-none"
-/>
+        <input
+          v-model="password"
+          type="password"
+          placeholder="Password"
+          class="w-full border rounded-lg p-2 focus:ring-2 focus:ring-blue-400 outline-none"
+        />
 
-<input
-v-model="password"
-type="password"
-placeholder="Password"
-class="w-full border rounded-lg p-2 focus:ring-2 focus:ring-blue-400 outline-none"
-/>
+        <button
+          @click="login"
+          class="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition"
+        >
+          Sign In
+        </button>
 
-<button
-@click="login"
-class="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition"
->
-Sign In
-</button>
+      </div>
 
-</div>
+    </div>
 
-</div>
-
-</div>
-
+  </div>
 </template>
 
 
@@ -63,15 +61,18 @@ async login(){
 try{
 
 const res = await axios.post(
-"http://localhost:5000/api/auth/login",
+"https://leave-management-system-backend-lh20.onrender.com/api/auth/login",
 {
 email:this.email,
 password:this.password
 }
 )
 
-// save login details
-
+// Save user details
+localStorage.setItem("token", res.data.token)
+localStorage.setItem("employeeId", res.data.user.employeeId)
+localStorage.setItem("userName", res.data.user.name)
+localStorage.setItem("role", res.data.user.role)
 
 // redirect based on role
 if(res.data.user.role === "employee"){
